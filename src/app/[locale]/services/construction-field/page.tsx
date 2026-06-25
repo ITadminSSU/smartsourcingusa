e76f722925ed9904654b1d842f5e626586466370
ITadminSSU <it-admin@smartsourcingusa.com>
2026-06-25 16:52:27 +0800

    Add files via upload

---
+++ src/app/[locale]/services/construction-field/page.tsx
@@ -0,0 +1,5 @@
+import ServicePageLayout from "@/components/ServicePageLayout";
+
+export default function ConstructionFieldPage() {
+  return <ServicePageLayout namespace="services.constructionField" />;
+}
---
+++ src/app/[locale]/services/operations-admin/page.tsx
@@ -0,0 +1,5 @@
+import ServicePageLayout from "@/components/ServicePageLayout";
+
+export default function OperationsAdminPage() {
+  return <ServicePageLayout namespace="services.operationsAdmin" />;
+}
